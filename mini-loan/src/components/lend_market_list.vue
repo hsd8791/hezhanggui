934e6c617ea9e383ae1e2345b8827528e6166b29
huangsd <912143546@qq.com>
2017-11-27 12:06:11 +0800

    cannot multiple apply if not fill identification

--- mini-loan/src/components/lend_market_list.vue
+++ mini-loan/src/components/lend_market_list.vue
@@ -152,6 +152,16 @@ export default {
       }
     },
     toggleChoose(){
+      console.log('bus.cfgEssential.allFilled',bus.cfgEssential.allFilled)
+      if(!bus.cfgEssential.allFilled){
+        let r=this.remind
+        r.remindMsg='请至首页完成认证'
+        r.remindOpts=[{msg:'确定',callback:()=>{
+          publicFun.goPage('/index')
+        }}]
+        r.isShow=true
+        return
+      }
       console.log('this vm',this.$root)
       let b = this.choosing
       if(b){
